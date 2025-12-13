7e88e9e717a7d536bb0dc82c3dbd4c2fae3aede0
Amel20011 <amelcns8@gmail.com>
2025-12-14 05:56:35 +0800

    Create group-close.js

---
+++ plugins/group-close.js
@@ -0,0 +1,8 @@
+import { requireAdmin, requireGroup } from "../lib/permission.js";
+import { setAnnounce } from "../lib/group.js";
+
+export default {
+  name: "group-close",
+  async run() {},
+  onCommand: "close"
+};
