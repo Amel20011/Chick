0d15186487b7a7519fa7512adca85f385965b4a4
Amel20011 <amelcns8@gmail.com>
2025-12-14 05:56:09 +0800

    Create group-open.js

---
+++ plugins/group-open.js
@@ -0,0 +1,8 @@
+import { requireAdmin, requireGroup } from "../lib/permission.js";
+import { setAnnounce } from "../lib/group.js";
+
+export default {
+  name: "group-open",
+  async run() {},
+  onCommand: "open"
+};
