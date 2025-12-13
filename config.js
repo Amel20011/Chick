40fb155f3dbcda9259b05c00fc658e216273dc08
Amel20011 <amelcns8@gmail.com>
2025-12-14 05:49:40 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+export default {
+  ownerNumber: "+13658700681",
+  ownerName: "Liviaa🌷",
+  prefix: ["!", ".", "/"],
+  botName: "Liviaa🌷",
+  aesthetic: {
+    icons: "ᥫ᭡ ᯓᡣ𐭩 ⋆.𐙚 ̊ ⋆˚꩜｡ᥫ᭡.ִֶָ𓂃",
+    emojis: "🌷☘️💗🌹"
+  },
+  mode: "public", // "public" | "self"
+  antispam: {
+    cooldownMs: 2000,
+    maxBurst: 5
+  },
+ antilink: {
+    enabledByDefault: true,
+    warnLimit: 1,
+    kickOnLimit: true,
+    notifyDM: true
+  },
+  sessionDir: "./session",
+  media: {
+    menu: "./media/menu.jpg",
+    welcome: "./media/welcome.jpg",
+    qris: "./media/qris.jpg"
+  }
+};
